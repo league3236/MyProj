5568a8e4d79c0145d61c0e4fb2e44e946e470916
ksh <league3236@gmail.com>
2018-06-07 10:00:37 +0900

    setMassage added

--- src/MyMessage.java
+++ src/MyMessage.java
@@ -3,6 +3,9 @@ public class MyMessage {
 	public MyMessage(String message) {
 		this.message = message;
 	}
+	public SetMessage(String message) {
+		this.message = message;
+	}
 	public void print() {
 		System.out.println(message);
 		System.out.println("¼öÁ¤");
